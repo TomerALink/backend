bb622a7d686151fbb98ff8374fe7f6d854707a59
Tomer Neeman <tomer@alink.co.il>
2024-11-24 23:01:34 +0200

    add cors origin for google auth

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ if (process.env.NODE_ENV === 'production') {
         origin: [   'http://127.0.0.1:3000',
                     'http://localhost:3000',
                     'http://127.0.0.1:5173',
-                    'http://localhost:5173'
+                    'http://localhost:5173',
+                    'https://staybnb-m8or.onrender.com'
                 ],
         credentials: true
     }
